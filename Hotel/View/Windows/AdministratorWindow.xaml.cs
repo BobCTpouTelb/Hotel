3930f5e62506b84e21959f2999b3a48127c6afa0
Bob_CTpOuTelb <mikle0373@gmail.com>
2024-10-08 10:41:49 +0300

    Подготовил проект к работе, добавил окно авторизации, администратора и смены пароля

--- Hotel/View/Windows/AdministratorWindow.xaml.cs
+++ Hotel/View/Windows/AdministratorWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Hotel
+namespace Hotel.View.Windows
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для AdministratorWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AdministratorWindow : Window
     {
-        public MainWindow()
+        public AdministratorWindow()
         {
             InitializeComponent();
         }
