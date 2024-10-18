90aaf747a3c150d115dc07af26e31e999d04e571
Bob_CTpOuTelb <mikle0373@gmail.com>
2024-10-18 16:30:32 +0300

    Изменил структуру авторизации, добавил блокировку

--- Hotel/View/Windows/AuthorizationWindow.xaml.cs
+++ Hotel/View/Windows/AuthorizationWindow.xaml.cs
@@ -23,55 +23,89 @@ namespace Hotel.View.Windows
         public AuthorizationWindow()
         {
             InitializeComponent();
+            BlockUserByDate();
         }
 
         private void EntryBTN_Click(object sender, RoutedEventArgs e)
         {
-            if(LoginTB.Text == string.Empty && PasswordPB.Password == string.Empty)
+            if (Validation() == true)
+            {
+                Authentication();
+            }
+        }
+
+        /// <summary>
+        /// Метод для проверки на наличие данных в полях для ввода.
+        /// </summary>
+        public bool Validation()
+        {
+            if (LoginTB.Text == string.Empty && PasswordPB.Password == string.Empty)
             {
                 Feedback.Warning("Поля для ввода логина и пароля обязательны для заполнения. Введитите логин и пароль.");
+                return false;
             }
-            else if(LoginTB.Text == string.Empty)
+            else if (LoginTB.Text == string.Empty)
             {
                 Feedback.Warning("Поле для ввода логина обязательно для заполнения. Введите логин.");
+                return false;
             }
             else if (PasswordPB.Password == string.Empty)
             {
                 Feedback.Warning("Поле для ввода пароля обязательно для заполнения. Введите пароль.");
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Метод для проверки учетных данных пользователя.
+        /// </summary>
+        public void Authentication()
+        {
+            // Поля для ввода логина и пароля заполенены, требуется проверка введённых данных.
+            App.currentUser = App.context.User.FirstOrDefault(user => user.Login == LoginTB.Text && user.Password == PasswordPB.Password);
+
+            if (App.currentUser == null)
+            {
+                Feedback.Error("Вы ввели неверный логин или пароль. Пожалуйста проверьте ещё раз введенные данные");
+            }
+            else if (App.currentUser.IsBlocked == true)
+            {
+                Feedback.Error("Вы заблокированы. Обратитесь к администратору.");
+            }
+            else if (App.currentUser.IsActivated == false)
+            {
+                ChagePasswordWindow chagePasswordWindow = new ChagePasswordWindow();
+                chagePasswordWindow.Show();
+                Hide();
             }
             else
             {
-                // Поля для ввода логина и пароля заполенены, требуется проверка введённых данных.
-                App.currentUser = App.context.User.FirstOrDefault(user => user.Login == LoginTB.Text && user.Password == PasswordPB.Password);
+                Feedback.Info("Вы успешно авторизовались.");
 
-                if(App.currentUser == null)
+                switch (App.currentUser.RoleId)
                 {
-                    Feedback.Error("Вы ввели неверный логин или пароль. Пожалуйста проверьте ещё раз введенные данные");
+                    case 1:
+                        AdministratorWindow administratorWindow = new AdministratorWindow();
+                        break;
+                    case 2:
+                        break;
                 }
-                else if (App.currentUser.IsBlocked == false)
-                {
-                    Feedback.Error("Вы заблокированы. Обратитесь к администратору.");
-                }
-                else if (App.currentUser.IsActivated == false)
-                {
-                    ChagePasswordWindow chagePasswordWindow = new ChagePasswordWindow();
-                    chagePasswordWindow.Show();
-                    Hide();
-                }
-                else
-                {
-                    Feedback.Info("Вы успешно авторизовались.");
-                    
-                    switch (App.currentUser.RoleId)
-                    {
-                        case 1:
-                            AdministratorWindow administratorWindow = new AdministratorWindow();
-                            break;
-                        case 2:
-                            break;
-                    }
 
-                    Close();
+                Close();
+            }
+        }
+        /// <summary>
+        /// Блокировка пользователя по дате.
+        /// </summary>
+        private void BlockUserByDate()
+        {
+            foreach (var user in App.context.User)
+            {
+                if (user.RegistrationDate.AddMonths(1) < DateTime.Now)
+                {
+                    user.IsBlocked = true;
                 }
             }
         }
